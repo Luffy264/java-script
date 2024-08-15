fc21a95bc40d180ce7a10b607f5bebdf2faf831f
Luffy264 <straw.hatrajan@gmail.com>
2024-08-15 10:51:44 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let name ="Rajan"
+let age =20
+let isLoggendId = false
+
+
+// number = 2 power 53
+//bigint
+// string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined =>
+    //symbol
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
